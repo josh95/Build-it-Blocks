d14814f3f3ec77f86b35f6e31f3ce2287163822b
Josh <jjr95@cornell.edu>
2013-08-23 11:59:43 -0400

    Edit instructions works!
    the image path updates. I had to add an enctype to the form. Forgot
    about this. it is necessary for forms that submit files

--- dev/edit-instructions-action.php
+++ dev/edit-instructions-action.php
@@ -1,44 +1,51 @@
 <?php
 		$numberofsteps = $_POST["numberofsteps"];
 		$moduleid = $_POST["moduleid"];
+		$typeid = $_POST["typeid"];
+		$typearray = array(
+			1 => "junk",
+			2 => "lego",
+			3 => "art",
+			4 => "code",
+			5 => "minecraft"
+		);
+		$typename= $typearray[$typeid];
 		include("../db-connect.php");
 		
 		$i=0; //a counter loop iterates a number of times equal to the number of steps
 		while($i<$numberofsteps){
-		$i++;
-		if ($_POST['textstep'.$i] != null){
-			$step= $_POST['textstep'.$i];
-			echo "changed Step 1 to " . $step. "<br>";
-			mysqli_query($con, "UPDATE `builditblocks`.`steps` SET `step-description` = \"". $step . "\" WHERE `moduleID` =".$moduleid. " AND `step-number`=".$i); //moduleID as given by previous page
-		}
-		else
-			echo "No change to step $i <br>";
-		}
+			$i++;
+			if ($_POST['textstep'.$i] != null){
+				$step= $_POST['textstep'.$i];
+				echo "changed Step 1 to " . $step. "<br>";
+				mysqli_query($con, "UPDATE `builditblocks`.`steps` SET `step-description` = \"". $step . "\" WHERE `moduleID` =".$moduleid. " AND `step-number`=".$i); //moduleID as given by previous page
+			}
+			else
+				echo "No change to step $i <br>";
+		
 
 
-		/*$allowedExts = array("gif", "jpeg", "jpg", "png");
-		$temp = explode(".", $_FILES["icon"]["name"]);
-		$extension = end($temp);
+			if ($_FILES["imagestep" . $i]["size"] < 2000000){ //if icon is larger than 1950kb, then it is too big. Tell user that icon is too big
+				if ($_FILES["imagestep" . $i]["size"] == 0){ //if icon field was left blank
+					echo "No change to step". $i ."image" . "<br>";
+				}
+				else{
+					//tell user some stats about their uploaded icon
+					echo "Upload: " . $_FILES["imagestep". $i]["name"] . "<br>"; 
+					echo "Type: " . $_FILES["imagestep". $i]["type"] . "<br>";
+					echo "Size: " . ($_FILES["imagestep" .$i]["size"] / 1024) . " kB<br>";
 
-		if ($_FILES["icon"]["size"] < 200000){ //if icon is larger than 195kb, then it is too big. Tell user that icon is too big
-			if ($_FILES["icon"]["size"] == 0){ //if icon field was left blank
-				echo "No change to icon" . "<br>";
+					move_uploaded_file($_FILES["imagestep".$i]["tmp_name"],
+					"upload/" . $_FILES["imagestep".$i]["name"]);
+					//update steps with new file path to image
+					mysqli_query($con, "UPDATE `builditblocks`.`steps` SET `image-path` = \"module-images/instruction-img/". $typename ."/" . $_FILES["imagestep".$i]["name"] . "\" WHERE `moduleID` =".$moduleid. " AND `step-number`=".$i);
+					echo "Stored in: " . "upload/" . $_FILES["imagestep1"]["name"];//stored in folder upload, will change to proper folder later
+					echo "<br>";
+				}
 			}
 			else{
-				//tell user some stats about their uploaded icon
-				echo "Upload: " . $_FILES["icon"]["name"] . "<br>"; 
-				echo "Type: " . $_FILES["icon"]["type"] . "<br>";
-				echo "Size: " . ($_FILES["icon"]["size"] / 1024) . " kB<br>";
-
-				move_uploaded_file($_FILES["icon"]["tmp_name"],
-				"upload/" . $_FILES["icon"]["name"]);
-				//update module-index with new file path to icon
-				mysqli_query($con, "UPDATE `builditblocks`.`module_index` SET `icon` = \" module-images/icons/" . $_FILES["icon"]["name"] . "\" WHERE `module_index`.`ID` =".$_POST['moduleid']);
-				echo "Stored in: " . "upload/" . $_FILES["icon"]["name"];//stored in folder upload, will change to proper folder later
+			  echo "Invalid icon. File size too big.";
 			}
 		}
-		else{
-		  echo "Invalid icon. File size too big.";
-		}*/
 
 ?>
